024d3c95d2a0fca253100021be3b4f1eed8d48fd
ethanryan <ethanryan@gmail.com>
2017-11-22 00:48:57 -0500

    tweaked supersimpletheme files

--- wp-content/themes/supersimpletheme/functions.php
+++ wp-content/themes/supersimpletheme/functions.php
@@ -3,12 +3,12 @@
 function bootstrapstarter_enqueue_styles() {
     wp_register_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
     $dependencies = array('bootstrap');
-    wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_uri(), $dependencies );
+	wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_uri(), $dependencies );
 }
 
 function bootstrapstarter_enqueue_scripts() {
     $dependencies = array('jquery');
-    wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '3.3.6', true );
+    wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_styles' );
@@ -20,4 +20,40 @@ function bootstrapstarter_wp_setup() {
 
 add_action( 'after_setup_theme', 'bootstrapstarter_wp_setup' );
 
+function bootstrapstarter_register_menu() {
+    register_nav_menu('header-menu', __( 'Header Menu' ));
+}
+add_action( 'init', 'bootstrapstarter_register_menu' );
+
+function bootstrapstarter_widgets_init() {
+
+    register_sidebar( array(
+        'name'          => 'Footer - Copyright Text',
+        'id'            => 'footer-copyright-text',
+        'before_widget' => '<div class="footer_copyright_text">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4>',
+        'after_title'   => '</h4>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => 'Sidebar - Inset',
+        'id'            => 'sidebar-1',
+        'before_widget' => '<div class="sidebar-module sidebar-module-inset">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4>',
+        'after_title'   => '</h4>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => 'Sidebar - Default',
+        'id'            => 'sidebar-2',
+        'before_widget' => '<div class="sidebar-module">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4>',
+        'after_title'   => '</h4>',
+    ) );
+
+}
+add_action( 'widgets_init', 'bootstrapstarter_widgets_init' );
 ?>
--- wp-content/themes/supersimpletheme/header.php
+++ wp-content/themes/supersimpletheme/header.php
@@ -17,21 +17,16 @@
 
 <div class="blog-masthead">
     <div class="container">
-        <nav class="blog-nav">
-            <a class="blog-nav-item active" href="#">Home</a>
-            <a class="blog-nav-item" href="#">New features</a>
-            <a class="blog-nav-item" href="#">Press</a>
-            <a class="blog-nav-item" href="#">New hires</a>
-            <a class="blog-nav-item" href="#">About</a>
-        </nav>
+        <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' => 'blog-nav list-inline'  ) ); ?>
     </div>
 </div>
 
 <div class="container">
 
     <div class="blog-header">
-        <h1 class="blog-title">The Bootstrap Blog</h1>
-        <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+        <h1 class="blog-title"><?php bloginfo( 'name' ); ?></h1>
+        <?php $description = get_bloginfo( 'description', 'display' ); ?>
+        <?php if($description) { ?><p class="lead blog-description"><?php echo $description ?></p><?php } ?>
     </div>
 
     <div class="row">
